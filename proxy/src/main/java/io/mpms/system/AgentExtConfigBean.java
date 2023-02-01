676004df1decb764fce007a9cfba4fdfe9e44d20
zywang_dev <dev12101@linx-info.com>
2023-02-01 14:21:39 +0800

    新建配置实现类

---
+++ proxy/src/main/java/io/mpms/system/AgentExtConfigBean.java
@@ -0,0 +1,23 @@
+package io.mpms.system;
+
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
+import cn.jiangzeyin.common.spring.SpringUtil;
+import io.jpom.common.ServerOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * proxy端外部配置
+ */
+@Configuration
+public class AgentExtConfigBean {
+
+
+}
