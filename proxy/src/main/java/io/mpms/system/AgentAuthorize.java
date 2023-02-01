57c70ba9d2d2b67adfe95afb43dd6cbf39b57c39
zywang_dev <dev12101@linx-info.com>
2023-02-01 14:01:16 +0800

    新建proxy账号信息类

---
+++ proxy/src/main/java/io/mpms/system/AgentAuthorize.java
@@ -0,0 +1,26 @@
+package io.mpms.system;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.jiangzeyin.common.spring.SpringUtil;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.model.system.AgentAutoUser;
+import io.jpom.util.JsonFileUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * proxy端授权账号信息
+ *
+ 
+
+ */
+@Configuration
+public class AgentAuthorize {
+
+
+}
