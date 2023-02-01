910a0a024294928701d9582326a5fd5c2ad90bf8
zywang_dev <dev12101@linx-info.com>
2023-02-01 13:58:25 +0800

    添加proxy端请求aop账号信息检测

---
+++ proxy/src/main/java/io/mpms/system/AgentAopLog.java
@@ -0,0 +1,19 @@
+package io.mpms.system;
+
+import io.jpom.common.BaseAgentController;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * 自动记录日志
+ *
+ */
+public class AgentAopLog {
+
+}
+
