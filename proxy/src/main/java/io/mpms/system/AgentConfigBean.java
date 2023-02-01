4a296967435b0e84d6aed44f304cf2a1014271e1
zywang_dev <dev12101@linx-info.com>
2023-02-01 14:20:09 +0800

    新建外部配置类

---
+++ proxy/src/main/java/io/mpms/system/AgentConfigBean.java
@@ -0,0 +1,20 @@
+package io.mpms.system;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.jiangzeyin.common.spring.SpringUtil;
+import io.jpom.common.BaseAgentController;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+/**
+ * 插件端配置
+ *
+
+
+ */
+@Configuration
+public class AgentConfigBean {
+
+}
