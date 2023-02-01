f21b2485db5744ecda9496abe308bfcf3aab2324
zywang_dev <dev12101@linx-info.com>
2023-02-01 10:04:56 +0800

    新建websocke配置类

---
+++ proxy/src/main/java/io/mpms/socket/AgentFileTailWatcher.java
@@ -0,0 +1,20 @@
+package io.mpms.socket;
+
+import cn.hutool.core.io.FileUtil;
+import cn.jiangzeyin.common.DefaultSystemLog;
+import io.jpom.util.BaseFileTailWatcher;
+
+import javax.websocket.Session;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 文件跟随器
+ *
+ */
+public class AgentFileTailWatcher<T> extends BaseFileTailWatcher<T> {
+
+}
