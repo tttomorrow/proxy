f347a4c1f1ac79c9c52ba75d64c8bc08aa8216ef
zywang_dev <dev12101@linx-info.com>
2023-02-01 14:57:05 +0800

    新建文件工具类

---
+++ proxy/src/main/java/io/mpms/util/FileUtils.java
@@ -0,0 +1,18 @@
+package io.mpms.util;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.system.SystemUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.File;
+
+/**
+ * 文件工具
+ */
+public class FileUtils {
+
+
+}
