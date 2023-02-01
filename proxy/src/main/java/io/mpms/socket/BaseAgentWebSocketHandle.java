7b44bb4f04f195e75f4908aa08085d75c86964b5
zywang_dev <dev12101@linx-info.com>
2023-02-01 13:52:05 +0800

    新建websock handler基类

---
+++ proxy/src/main/java/io/mpms/socket/BaseAgentWebSocketHandle.java
@@ -0,0 +1,28 @@
+package io.mpms.socket;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.exceptions.ExceptionUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
+import cn.jiangzeyin.common.DefaultSystemLog;
+import io.jpom.system.AgentAuthorize;
+import io.jpom.system.ConfigBean;
+import io.jpom.util.SocketSessionUtil;
+
+import javax.websocket.CloseReason;
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static javax.websocket.CloseReason.CloseCodes.CANNOT_ACCEPT;
+
+/**
+ * 插件端socket 基类
+ *
+ */
+public abstract class BaseAgentWebSocketHandle {
+
+
+}
