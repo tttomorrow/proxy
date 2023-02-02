1d477d1d172316c925cbc3eda05a7c26c4b97189
zywang_dev <dev12101@linx-info.com>
2023-02-02 17:11:17 +0800

    添加配置内容捕获变量

--- proxy/src/main/java/io/mpms/system/AgentExtConfigBean.java
+++ proxy/src/main/java/io/mpms/system/AgentExtConfigBean.java
@@ -18,6 +18,85 @@ import java.util.Map;
  */
 @Configuration
 public class AgentExtConfigBean {
+    private static AgentExtConfigBean agentExtConfigBean;
+    /**
+     * 白名单路径是否判断包含关系
+     */
+    @Value("${whitelistDirectory.checkStartsWith:true}")
+    public boolean whitelistDirectoryCheckStartsWith;
 
+    /**
+     * 自动备份控制台日志，防止日志文件过大，目前暂只支持linux 不停服备份  如果配置none 则不自动备份 默认10分钟扫描一次
+     */
+    @Value("${log.autoBackConsoleCron:0 0/10 * * * ?}")
+    public String autoBackConsoleCron;
+    /**
+     * 当文件多大时自动备份
+     *
+     * @see ch.qos.logback.core.util.FileSize
+     */
+    @Value("${log.autoBackSize:50MB}")
+    public String autoBackSize;
+    /**
+     * 控制台日志保存时长单位天
+     */
+    @Value("${log.saveDays:7}")
+    private int logSaveDays;
+
+    @Value("${mpms.agent.id:}")
+    private String agentId;
+
+    @Value("${mpms.agent.url:}")
+    private String agentUrl;
+
+    @Value("${mpms.server.url:}")
+    private String serverUrl;
+
+    @Value("${mpms.server.token:}")
+    private String serverToken;
+
+    /**
+     * 项目状态禁用 调用jmx获取
+     */
+    @Value("${project.disableVirtualMachine:false}")
+    private boolean disableVirtualMachine;
+
+    /**
+     * 停止项目等待的时长 单位秒，最小为1秒
+     */
+    @Value("${project.stopWaitTime:10}")
+    private int stopWaitTime;
+
+    /**
+     * 包构架信息
+     */
+    @Value("${packageArchitecture.amd64:all amd64 i386}")
+    public String amd64Arch;
+
+    @Value("${packageArchitecture.arm64:all arm64}")
+    public String arm64Arch;
+
+    @Value("${packageArchitecture.i386:all i386}")
+    public String i386Arch;
+
+    public int getStopWaitTime() {
+        return stopWaitTime;
+    }
+
+    public boolean isDisableVirtualMachine() {
+        return disableVirtualMachine;
+    }
+
+    public String getAgentId() {
+        return agentId;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public String getServerToken() {
+        return serverToken;
+    }
 
 }
