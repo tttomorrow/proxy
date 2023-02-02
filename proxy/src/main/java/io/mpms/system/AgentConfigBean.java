bd7c55691c282a85abe4ada15768c37de5a7499f
zywang_dev <dev12101@linx-info.com>
2023-02-02 17:09:22 +0800

    添加外部配置变量

--- proxy/src/main/java/io/mpms/system/AgentConfigBean.java
+++ proxy/src/main/java/io/mpms/system/AgentConfigBean.java
@@ -16,5 +16,47 @@ import java.io.File;
  */
 @Configuration
 public class AgentConfigBean {
+    /**
+     * 白名单文件
+     */
+    public static final String WHITELIST_DIRECTORY = "whitelistDirectory.json";
+    /**
+     * 项目数据文件
+     */
+    public static final String PROJECT = "project.json";
 
+    public static final String TOMCAT = "tomcat.json";
+    /**
+     * 项目回收文件
+     */
+    public static final String PROJECT_RECOVER = "project_recover.json";
+    /**
+     * 证书文件
+     */
+    public static final String CERT = "cert.json";
+    /**
+     * 脚本管理数据文件
+     */
+    public static final String SCRIPT = "script.json";
+    /**
+     * 脚本模板存放路径
+     */
+    public static final String SCRIPT_DIRECTORY = "script";
+
+    /**
+     * Server 端的信息
+     */
+    public static final String SERVER_ID = "SERVER.json";
+
+    /**
+     * nginx配置信息
+     */
+    public static final String NGINX_CONF = "nginx_conf.json";
+
+    /**
+     * jdk列表信息
+     */
+    public static final String JDK_CONF = "jdk_conf.json";
+
+    private static AgentConfigBean agentConfigBean;
 }
