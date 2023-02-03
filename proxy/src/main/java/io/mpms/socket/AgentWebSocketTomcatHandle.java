e523052bbb2050b0c9b8394e7d11ac02a1db4c3f
zywang_dev <dev12101@linx-info.com>
2023-02-03 14:39:13 +0800

    添加tomcat开启

--- proxy/src/main/java/io/mpms/socket/AgentWebSocketTomcatHandle.java
+++ proxy/src/main/java/io/mpms/socket/AgentWebSocketTomcatHandle.java
@@ -31,4 +31,32 @@ public class AgentWebSocketTomcatHandle extends BaseAgentWebSocketHandle {
     private TomcatEditService tomcatEditService;
 
     private static final Map<String, File> CACHE_FILE = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        try {
+            if (super.checkAuthorize(session)) {
+                return;
+            }
+            String tomcatId = super.getParameters(session, "tomcatId");
+            if (tomcatEditService == null) {
+                tomcatEditService = SpringUtil.getBean(TomcatEditService.class);
+            }
+            TomcatInfoModel tomcatInfoModel = tomcatEditService.getItem(tomcatId);
+            if (tomcatInfoModel == null && !JpomApplication.SYSTEM_ID.equalsIgnoreCase(tomcatId)) {
+                SocketSessionUtil.send(session, "获取tomcat信息错误");
+                session.close();
+                return;
+            }
+            SocketSessionUtil.send(session, "连接成功：" + (tomcatInfoModel == null ? "" : tomcatInfoModel.getName()));
+        } catch (Exception e) {
+            DefaultSystemLog.getLog().error("socket 错误", e);
+            try {
+                SocketSessionUtil.send(session, JsonMessage.getString(500, "系统错误!"));
+                session.close();
+            } catch (IOException e1) {
+                DefaultSystemLog.getLog().error(e1.getMessage(), e1);
+            }
+        }
+    }
 }
