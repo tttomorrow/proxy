45f953547728f224d32a4fdd95f8dd6dea876bac
zywang_dev <dev12101@linx-info.com>
2023-02-23 14:27:11 +0800

    日志数据服务

---
+++ proxy/src/main/java/io/mpms/service/mysqldb/impl/MiniSysLogServiceImpl.java
@@ -0,0 +1,24 @@
+package io.mpms.service.mysqldb.impl;
+
+import cn.hutool.core.date.DateTime;
+import io.jpom.dao.MiniSysLogDao;
+import io.jpom.model.data.MiniSysLog;
+import io.jpom.service.mysqldb.MiniSysLogService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * 凝思系统软件安装管理平台系统日志表(Minisyslog)表服务实现类
+ *
+ * 
+ * @since 2021-10-22 09:53:54
+ */
+@Service("miniSysLogService")
+public class MiniSysLogServiceImpl implements MiniSysLogService {
+
+}
