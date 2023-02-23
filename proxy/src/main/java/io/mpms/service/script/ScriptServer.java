56b35a4838effd9e44e6007cd9acf4a60528c22e
zywang_dev <dev12101@linx-info.com>
2023-02-23 14:20:59 +0800

    新建脚本管理

---
+++ proxy/src/main/java/io/mpms/service/script/ScriptServer.java
@@ -0,0 +1,18 @@
+package io.mpms.service.script;
+
+import cn.hutool.core.io.FileUtil;
+import io.jpom.common.BaseOperService;
+import io.jpom.model.data.ScriptModel;
+import io.jpom.system.AgentConfigBean;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 脚本模板管理
+ */
+@Service
+public class ScriptServer extends BaseOperService<ScriptModel> {
+
+
+}
