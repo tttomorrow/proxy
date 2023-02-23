114887b3ec895de251dd288ec77d85b7192fa89d
zywang_dev <dev12101@linx-info.com>
2023-02-23 14:37:22 +0800

    延时任务表服务

---
+++ proxy/src/main/java/io/mpms/service/mysqldb/DelayedTaskService.java
@@ -0,0 +1,19 @@
+package io.mpms.service.mysqldb;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.model.data.DelayedTask;
+
+import java.util.List;
+
+/**
+ * (Delayedtask)表服务接口
+ *
+ *
+ * @since 2021-10-21 14:34:09
+ */
+public interface DelayedTaskService {
+
+
+
+}
