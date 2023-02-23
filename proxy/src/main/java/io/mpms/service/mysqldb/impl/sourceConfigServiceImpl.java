d6828b54add735acdc04277e90e17461434ed684
zywang_dev <dev12101@linx-info.com>
2023-02-23 14:26:20 +0800

    源配置数据服务

---
+++ proxy/src/main/java/io/mpms/service/mysqldb/impl/sourceConfigServiceImpl.java
@@ -0,0 +1,20 @@
+package io.mpms.service.mysqldb.impl;
+
+import io.jpom.dao.SourceConfigDao;
+import io.jpom.model.system.SourceConfig;
+import io.jpom.service.mysqldb.SourceConfigService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * (Sourceconfig)表服务实现类
+ *
+ *
+ * @since 2021-10-25 17:05:11
+ */
+@Service("sourceconfigService")
+public class sourceConfigServiceImpl implements SourceConfigService {
+
+}
