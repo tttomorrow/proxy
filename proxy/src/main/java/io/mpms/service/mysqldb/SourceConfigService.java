b9bee9398d2bcadda0071d6a072bb2ed6a016e49
zywang_dev <dev12101@linx-info.com>
2023-02-23 14:22:36 +0800

    原配置data类

---
+++ proxy/src/main/java/io/mpms/service/mysqldb/SourceConfigService.java
@@ -0,0 +1,17 @@
+package io.mpms.service.mysqldb;
+
+import io.jpom.model.system.SourceConfig;
+
+import java.util.List;
+
+/**
+ * (Sourceconfig)表服务接口
+ *
+ *
+ * @since 2021-10-25 17:05:11
+ */
+public interface SourceConfigService {
+
+
+
+}
