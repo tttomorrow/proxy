02f799658060ea213f63f45b768827479d826ed2
zywang_dev <dev12101@linx-info.com>
2023-02-23 14:39:55 +0800

    tomcat配置表服务

---
+++ proxy/src/main/java/io/mpms/service/manage/TomcatEditService.java
@@ -0,0 +1,15 @@
+package io.mpms.service.manage;
+
+import cn.hutool.core.date.DateUtil;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.common.BaseOperService;
+import io.jpom.model.data.TomcatInfoModel;
+import io.jpom.system.AgentConfigBean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TomcatEditService extends BaseOperService<TomcatInfoModel> {
+
+
+
+}
