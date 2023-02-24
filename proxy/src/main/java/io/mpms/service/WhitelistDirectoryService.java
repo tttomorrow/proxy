b2208ef6d07d73df9bae0ed9e98882bd33cb4eed
zywang_dev <dev12101@linx-info.com>
2023-02-24 15:29:01 +0800

    白名单管理

---
+++ proxy/src/main/java/io/mpms/service/WhitelistDirectoryService.java
@@ -0,0 +1,21 @@
+package io.mpms.service;
+
+import cn.jiangzeyin.common.DefaultSystemLog;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.common.BaseDataService;
+import io.jpom.model.data.AgentWhitelist;
+import io.jpom.system.AgentConfigBean;
+import io.jpom.util.JsonFileUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 白名单服务
+ */
+@Service
+public class WhitelistDirectoryService extends BaseDataService {
+
+
+}
