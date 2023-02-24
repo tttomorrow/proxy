25c95872d13005dbf7d59c02510a7c438a80f80c
zywang_dev <dev12101@linx-info.com>
2023-02-24 15:45:32 +0800

    更新在线升级依赖

--- proxy/src/main/java/io/mpms/socket/AgentWebSocketUpdateHandle.java
+++ proxy/src/main/java/io/mpms/socket/AgentWebSocketUpdateHandle.java
@@ -2,7 +2,6 @@ package io.mpms.socket;
 
 import cn.jiangzeyin.common.DefaultSystemLog;
 import com.alibaba.fastjson.JSONObject;
-import io.jpom.JpomApplication;
 import io.jpom.common.JpomManifest;
 import io.jpom.model.AgentFileModel;
 import io.jpom.model.WebSocketMessageModel;
