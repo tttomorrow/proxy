6b73ae2588912a96d15d3c58baf86e51a87ba06b
zywang_dev <dev12101@linx-info.com>
2023-02-24 15:25:20 +0800

    延时任务线程

---
+++ proxy/src/main/java/io/mpms/service/manage/DelayedTaskRunner.java
@@ -0,0 +1,17 @@
+package io.mpms.service.manage;
+
+import cn.jiangzeyin.common.DefaultSystemLog;
+import io.jpom.common.commander.AbstractSystemCommander;
+import io.jpom.model.data.DelayedTask;
+import io.jpom.model.data.SystemCommanderResult;
+import io.jpom.service.mysqldb.DelayedTaskService;
+import io.jpom.service.mysqldb.MiniSysLogService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class DelayedTaskRunner implements Runnable {
+
+
+}
