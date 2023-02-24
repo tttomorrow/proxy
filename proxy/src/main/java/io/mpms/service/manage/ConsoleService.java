45b11e919962dbe61fb7d69209028848ee8c8d5e
zywang_dev <dev12101@linx-info.com>
2023-02-24 15:26:29 +0800

    远程控制台管理

---
+++ proxy/src/main/java/io/mpms/service/manage/ConsoleService.java
@@ -0,0 +1,17 @@
+package io.mpms.service.manage;
+
+import cn.hutool.core.date.DateUtil;
+import io.jpom.common.commander.AbstractProjectCommander;
+import io.jpom.model.data.ProjectInfoModel;
+import io.jpom.socket.ConsoleCommandOp;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 控制台
+ */
+@Service
+public class ConsoleService {
+
+}
