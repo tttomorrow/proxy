b97f3e51ef2564e15c1cea1fe72d93c5ea5feb95
zywang_dev <dev12101@linx-info.com>
2023-02-24 15:20:18 +0800

    项目管理服务

---
+++ proxy/src/main/java/io/mpms/service/manage/ProjectRecoverService.java
@@ -0,0 +1,17 @@
+package io.mpms.service.manage;
+
+import cn.hutool.core.date.DateUtil;
+import io.jpom.common.BaseOperService;
+import io.jpom.model.data.ProjectRecoverModel;
+import io.jpom.system.AgentConfigBean;
+import org.springframework.stereotype.Service;
+
+/**
+ * 项目管理
+ *
+ */
+@Service
+public class ProjectRecoverService extends BaseOperService<ProjectRecoverModel> {
+
+
+}
