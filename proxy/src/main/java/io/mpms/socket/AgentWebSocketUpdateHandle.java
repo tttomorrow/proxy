19fac03351a39a705c1444664812765a869681c3
zywang_dev <dev12101@linx-info.com>
2023-03-01 16:55:51 +0800

    在线升级依赖更新

--- proxy/src/main/java/io/mpms/socket/AgentWebSocketUpdateHandle.java
+++ proxy/src/main/java/io/mpms/socket/AgentWebSocketUpdateHandle.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.JSONObject;
 import io.jpom.common.JpomManifest;
 import io.jpom.model.AgentFileModel;
 import io.jpom.model.WebSocketMessageModel;
-import io.jpom.model.data.UploadFileModel;
 import io.jpom.system.AgentConfigBean;
 import io.jpom.util.SocketSessionUtil;
 import org.springframework.stereotype.Component;
