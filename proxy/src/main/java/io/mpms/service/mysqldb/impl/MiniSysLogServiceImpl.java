ec36798f0d3cacb6718ec2d265bc543614911704
zywang_dev <dev12101@linx-info.com>
2023-03-02 16:56:17 +0800

    日志范围查询

--- proxy/src/main/java/io/mpms/service/mysqldb/impl/MiniSysLogServiceImpl.java
+++ proxy/src/main/java/io/mpms/service/mysqldb/impl/MiniSysLogServiceImpl.java
@@ -34,4 +34,16 @@ public class MiniSysLogServiceImpl implements MiniSysLogService {
     public MiniSysLog queryById(Integer id) {
         return this.minisyslogDao.queryById(id);
     }
+
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit  查询条数
+     * @return 对象列表
+     */
+    @Override
+    public List<MiniSysLog> queryAllByLimit(int offset, int limit) {
+        return this.minisyslogDao.queryAllByLimit(offset, limit);
+    }
 }
