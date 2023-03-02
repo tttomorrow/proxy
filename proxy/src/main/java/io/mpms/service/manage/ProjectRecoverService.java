04374fb05e14512f5f691d1ddbdfdd4a4b9adcaf
zywang_dev <dev12101@linx-info.com>
2023-03-02 15:15:58 +0800

    保存项目信息

--- proxy/src/main/java/io/mpms/service/manage/ProjectRecoverService.java
+++ proxy/src/main/java/io/mpms/service/manage/ProjectRecoverService.java
@@ -16,4 +16,16 @@ public class ProjectRecoverService extends BaseOperService<ProjectRecoverModel> 
     public ProjectRecoverService() {
         super(AgentConfigBean.PROJECT_RECOVER);
     }
+
+    /**
+     * 保存项目信息
+     *
+     * @param projectInfo 项目
+     */
+    @Override
+    public void addItem(ProjectRecoverModel projectInfo) {
+        projectInfo.setDelTime(DateUtil.now());
+        // 保存
+        super.addItem(projectInfo);
+    }
 }
