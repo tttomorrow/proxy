ccede6e42cd6df5988b1a847278ee633202dd23a
zywang_dev <dev12101@linx-info.com>
2023-03-03 14:55:43 +0800

    任务解析依赖更新

--- proxy/src/main/java/io/mpms/service/manage/TaskParser.java
+++ proxy/src/main/java/io/mpms/service/manage/TaskParser.java
@@ -3,7 +3,6 @@ package io.mpms.service.manage;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import io.jpom.model.TaskQueueModel;
 import io.jpom.model.data.DelayedTask;
 
 import java.util.ArrayList;
