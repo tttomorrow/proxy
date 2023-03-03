f1d151407119447fe5d6a064ed5389ad796e6697
zywang_dev <dev12101@linx-info.com>
2023-03-03 13:33:25 +0800

    jdk信息更新

--- proxy/src/main/java/io/mpms/service/manage/JdkInfoService.java
+++ proxy/src/main/java/io/mpms/service/manage/JdkInfoService.java
@@ -16,4 +16,20 @@ public class JdkInfoService extends BaseOperService<JdkInfoModel> {
         super(AgentConfigBean.JDK_CONF);
     }
 
+    /**
+     * 删除项目
+     *
+     * @param id 项目
+     */
+    @Override
+    public void deleteItem(String id) {
+        ProjectInfoModel projectInfo = getItem(id);
+        String userId = BaseAgentController.getNowUserName();
+        super.deleteItem(id);
+        // 添加回收记录
+        ProjectRecoverModel projectRecoverModel = new ProjectRecoverModel(projectInfo);
+        projectRecoverModel.setDelUser(userId);
+        projectRecoverService.addItem(projectRecoverModel);
+    }
+
 }
