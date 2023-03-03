fe5bdef01eb8340c429030f48292204f8e5c3adb
zywang_dev <dev12101@linx-info.com>
2023-03-03 11:34:19 +0800

    任务对象转json

--- proxy/src/main/java/io/mpms/service/manage/TaskParser.java
+++ proxy/src/main/java/io/mpms/service/manage/TaskParser.java
@@ -17,4 +17,21 @@ public class TaskParser {
     public TaskParser() {
 
     }
+
+    public JSONObject taskModelToJsonobject(DelayedTask delayedTask) {
+        JSONObject taskJsonObj = new JSONObject();
+
+        taskJsonObj.put("id", delayedTask.getId());
+        taskJsonObj.put("nodeId", delayedTask.getTaskNodeId());
+        taskJsonObj.put("taskStatus", delayedTask.getTaskStatus());
+        taskJsonObj.put("taskPriority", delayedTask.getTaskPriority());
+        taskJsonObj.put("taskGroupId", delayedTask.getTaskGroupId());
+        taskJsonObj.put("taskAction", delayedTask.getTaskAction());
+        taskJsonObj.put("taskTarget", delayedTask.getTaskTarget());
+        taskJsonObj.put("taskContent", delayedTask.getTaskContent());
+        taskJsonObj.put("taskViewTimes", delayedTask.getTaskViewTimes());
+        taskJsonObj.put("taskExtra", delayedTask.getTaskExtra());
+
+        return taskJsonObj;
+    }
 }
