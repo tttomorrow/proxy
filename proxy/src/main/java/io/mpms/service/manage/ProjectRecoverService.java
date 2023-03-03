ac81894572e92bec6c1f2ec206ce33b75d28e4ec
zywang_dev <dev12101@linx-info.com>
2023-03-03 09:40:02 +0800

    项目信息服务依赖更新

--- proxy/src/main/java/io/mpms/service/manage/ProjectRecoverService.java
+++ proxy/src/main/java/io/mpms/service/manage/ProjectRecoverService.java
@@ -1,7 +1,6 @@
 package io.mpms.service.manage;
 
 import cn.hutool.core.date.DateUtil;
-import io.jpom.common.BaseOperService;
 import io.jpom.model.data.ProjectRecoverModel;
 import io.jpom.system.AgentConfigBean;
 import org.springframework.stereotype.Service;
