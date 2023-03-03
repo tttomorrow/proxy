90d0bac378e98df6fb655e21a1845a53411f21a9
zywang_dev <dev12101@linx-info.com>
2023-03-03 16:37:26 +0800

    新建进程实例对象

---
+++ proxy/src/main/java/io/mpms/model/system/ProcessModel.java
@@ -0,0 +1,18 @@
+package io.mpms.model.system;
+
+import cn.hutool.core.util.StrUtil;
+import cn.jiangzeyin.common.DefaultSystemLog;
+import io.jpom.common.commander.AbstractProjectCommander;
+import io.jpom.model.BaseJsonModel;
+
+import java.io.IOException;
+
+/**
+ * 进程信息实体
+ *
+ 
+ 
+ */
+public class ProcessModel extends BaseJsonModel {
+
+}
