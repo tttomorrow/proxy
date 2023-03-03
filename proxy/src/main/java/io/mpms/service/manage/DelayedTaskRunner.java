5089294bb3265447986f8c188e146f7af3f1d82e
zywang_dev <dev12101@linx-info.com>
2023-03-03 15:00:22 +0800

    延时任务线程依赖更新

--- proxy/src/main/java/io/mpms/service/manage/DelayedTaskRunner.java
+++ proxy/src/main/java/io/mpms/service/manage/DelayedTaskRunner.java
@@ -1,7 +1,6 @@
 package io.mpms.service.manage;
 
 import cn.jiangzeyin.common.DefaultSystemLog;
-import io.jpom.common.commander.AbstractSystemCommander;
 import io.jpom.model.data.DelayedTask;
 import io.jpom.model.data.SystemCommanderResult;
 import io.jpom.service.mysqldb.DelayedTaskService;
