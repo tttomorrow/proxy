5947ee5fe327cfc749694892c17fe8bb15f6b1e2
zywang_dev <dev12101@linx-info.com>
2023-03-03 16:46:26 +0800

    新建项目信息对象

---
+++ proxy/src/main/java/io/mpms/model/data/ProjectInfoModel.java
@@ -0,0 +1,31 @@
+package io.mpms.model.data;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HtmlUtil;
+import cn.jiangzeyin.common.DefaultSystemLog;
+import cn.jiangzeyin.common.request.XssFilter;
+import cn.jiangzeyin.common.spring.SpringUtil;
+import io.jpom.common.commander.AbstractProjectCommander;
+import io.jpom.model.BaseJsonModel;
+import io.jpom.model.BaseModel;
+import io.jpom.model.RunMode;
+import io.jpom.service.WhitelistDirectoryService;
+import io.jpom.system.LinuxRuntimeException;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * 项目配置信息实体
+ *
+ */
+public class ProjectInfoModel extends BaseModel {
+
+}
