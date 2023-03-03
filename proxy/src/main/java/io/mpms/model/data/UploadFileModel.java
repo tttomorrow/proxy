a917d0ca8fafba24a1c8b2a2397eee419f5b5fe6
zywang_dev <dev12101@linx-info.com>
2023-03-03 16:39:56 +0800

    新建文件上传对象

---
+++ proxy/src/main/java/io/mpms/model/data/UploadFileModel.java
@@ -0,0 +1,15 @@
+package io.mpms.model.data;
+
+import cn.hutool.core.io.FileUtil;
+import io.jpom.model.BaseModel;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class UploadFileModel extends BaseModel {
+
+}
