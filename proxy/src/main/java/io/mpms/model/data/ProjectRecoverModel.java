04bb7b3e70e772930d063ab6664ff207823475ac
zywang_dev <dev12101@linx-info.com>
2023-03-03 16:45:46 +0800

    新建项目覆盖对象

---
+++ proxy/src/main/java/io/mpms/model/data/ProjectRecoverModel.java
@@ -0,0 +1,13 @@
+package io.mpms.model.data;
+
+import cn.hutool.core.lang.ObjectId;
+import io.jpom.model.BaseModel;
+
+/**
+ * 项目回收记录实体
+ *
+
+ */
+public class ProjectRecoverModel extends BaseModel {
+
+}
