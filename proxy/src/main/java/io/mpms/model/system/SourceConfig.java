f6f060388fa626b4099524127e9f2f0c9bded0f4
zywang_dev <dev12101@linx-info.com>
2023-03-03 16:36:40 +0800

    新建源配置对象

---
+++ proxy/src/main/java/io/mpms/model/system/SourceConfig.java
@@ -0,0 +1,14 @@
+package io.mpms.model.system;
+
+import java.io.Serializable;
+
+/**
+ * (SourceConfig)实体类
+ *
+ *
+ * @since 2021-10-25 17:05:11
+ */
+
+public class SourceConfig implements Serializable {
+
+}
