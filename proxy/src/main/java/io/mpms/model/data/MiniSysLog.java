a55b21cb060b6050d9d1385b0b88d23acdd7a0cc
zywang_dev <dev12101@linx-info.com>
2023-03-03 16:47:30 +0800

    新建日志对象

---
+++ proxy/src/main/java/io/mpms/model/data/MiniSysLog.java
@@ -0,0 +1,15 @@
+package io.mpms.model.data;
+
+import cn.hutool.core.date.DateTime;
+
+import java.io.Serializable;
+
+/**
+ * 凝思系统软件安装管理平台系统日志表(Minisyslog)实体类
+ *
+ *
+ * @since 2021-10-22 09:53:54
+ */
+public class MiniSysLog implements Serializable {
+
+}
