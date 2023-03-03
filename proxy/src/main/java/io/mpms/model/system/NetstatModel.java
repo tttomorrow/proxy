2b682cc20aa597bd6b92bb62c9a441952b423990
zywang_dev <dev12101@linx-info.com>
2023-03-03 16:38:07 +0800

    新建网络端口对象

---
+++ proxy/src/main/java/io/mpms/model/system/NetstatModel.java
@@ -0,0 +1,11 @@
+package io.mpms.model.system;
+
+import cn.hutool.core.util.StrUtil;
+import io.jpom.model.BaseJsonModel;
+
+/**
+ * 网络端口信息实体
+ */
+public class NetstatModel extends BaseJsonModel {
+
+}
