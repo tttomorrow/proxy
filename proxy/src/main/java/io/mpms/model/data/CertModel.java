93c35b4db0205af97aa7f59c6e819706391a29f5
zywang_dev <dev12101@linx-info.com>
2023-03-06 14:37:12 +0800

    证书文件名

--- proxy/src/main/java/io/mpms/model/data/CertModel.java
+++ proxy/src/main/java/io/mpms/model/data/CertModel.java
@@ -29,4 +29,9 @@ import java.util.Date;
 public class CertModel extends BaseModel {
 
     private static final String KEY = "Jpom 管理系统";
+
+    /**
+     * 证书文件
+     */
+    private String cert;
 }
